1a4422c49a9791d65f94d8be37ce72d4c4f30f83
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-04-23 03:13:00 +0530

    Q2

---
+++ Session-4/Q2.py
@@ -0,0 +1,7 @@
+def containsNearbyDuplicate(nums, k):
+    last_seen = {}
+    for i, num in enumerate(nums):
+        if num in last_seen and i - last_seen[num] <= k:
+            return True
+        last_seen[num] = i
+    return False
