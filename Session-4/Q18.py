c1ca41d594e030eb66d410dc6e5252a3d76b1e9c
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-04-23 03:13:12 +0530

    Q18

---
+++ Session-4/Q18.py
@@ -0,0 +1,15 @@
+def countBinarySubstrings(s):
+    groups = []
+    count = 1
+    for i in range(1, len(s)):
+        if s[i] == s[i-1]:
+            count += 1
+        else:
+            groups.append(count)
+            count = 1
+    groups.append(count)
+    
+    res = 0
+    for i in range(1, len(groups)):
+        res += min(groups[i], groups[i-1])
+    return res
