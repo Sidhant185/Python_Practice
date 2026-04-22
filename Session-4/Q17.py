48728811330efd4141bf447f137de68096a5fb92
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-04-23 03:13:12 +0530

    Q17

---
+++ Session-4/Q17.py
@@ -0,0 +1,14 @@
+def multiply(num1, num2):
+    if num1 == "0" or num2 == "0": return "0"
+    res = [0] * (len(num1) + len(num2))
+    for i in range(len(num1)-1, -1, -1):
+        for j in range(len(num2)-1, -1, -1):
+            mul = int(num1[i]) * int(num2[j])
+            p1, p2 = i + j, i + j + 1
+            total = mul + res[p2]
+            res[p2] = total % 10
+            res[p1] += total // 10
+            
+    start = 0
+    while start < len(res) and res[start] == 0: start += 1
+    return "".join(map(str, res[start:]))
