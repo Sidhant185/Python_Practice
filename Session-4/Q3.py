8547d53f19ac4f7710ab935bc8a20bd249500bc0
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-04-23 03:13:00 +0530

    Q3

---
+++ Session-4/Q3.py
@@ -0,0 +1,8 @@
+def canPlaceFlowers(flowerbed, k):
+    count = 0
+    f = [0] + flowerbed + [0] 
+    for i in range(1, len(f) - 1):
+        if f[i-1] == 0 and f[i] == 0 and f[i+1] == 0:
+            f[i] = 1
+            count += 1
+    return count >= k
