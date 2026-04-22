a75647eeee35ef78c78ebe12295a166639b7a300
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-04-23 03:13:04 +0530

    Q9

---
+++ Session-4/Q9.py
@@ -0,0 +1,9 @@
+def rotate(matrix):
+    n = len(matrix)
+    # Transpose
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    # Reverse Rows
+    for i in range(n):
+        matrix[i].reverse()
