ac9e8cb005bff29f03c9ff963d156a25a579203d
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-04-23 06:40:05 +0530

    Add comprehensive comments to Session-1/Q15.py

--- Session-1/Q15.py
+++ Session-1/Q15.py
@@ -1,14 +1,28 @@
+# Q15: Simple calculator with four operations
+# Performs addition, subtraction, multiplication, or division based on operator
+
+# Input: Get operator from user (take first character)
 operator = input("Enter Operator: ")[0]
+
+# Input: Get first number
 n1 = int(input("Enter a number: "))
+
+# Input: Get second number
 n2 = int(input("Enter a number: "))
 
+# Check operator and perform corresponding operation
 if(operator == '*'):
+    # Multiplication
     print(n1*n2)
 elif(operator == '/'):
+    # Division
     print(n1/n2)
 elif(operator == '+'):
+    # Addition
     print(n1+n2)
 elif(operator == '-'):
+    # Subtraction
     print(n1-n2)
 else:
+    # Invalid operator
     print("Invalid")
