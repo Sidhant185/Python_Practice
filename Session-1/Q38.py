78803348a8cc0bb7a71fab17075f1020ffabe797
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-04-23 06:40:06 +0530

    Add comprehensive comments to Session-1/Q38.py

--- Session-1/Q38.py
+++ Session-1/Q38.py
@@ -1,9 +1,18 @@
+# Q38: Function to check if number is even or odd
+# Returns string description of number type
+
+# Input: Get number from user
 num = int(input())
 
+# Define function to check if even or odd
 def check(num):
+    # Check remainder when divided by 2
     if(num%2==0):
+        # Number is even
         return "Even"
     else:
+        # Number is odd
         return "Odd"
 
+# Call function and print result
 print(check(num))
