c6c722e9ec74064f654abe88077fe6d40c0fc21c
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-04-23 06:40:05 +0530

    Add comprehensive comments to Session-1/Q24.py

--- Session-1/Q24.py
+++ Session-1/Q24.py
@@ -1,4 +1,10 @@
+# Q24: Print multiplication table of a number
+# Uses for loop to print products from 1 to 10 times
+
+# Input: Get number from user
 num = int(input("Enter a number: "))
 
+# Loop from 1 to 10 (inclusive)
 for i in range(1, 11):
+    # Print number multiplied by loop counter
     print(num*i)
