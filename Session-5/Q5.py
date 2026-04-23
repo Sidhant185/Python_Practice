1b02fc21bd562da62baa8e2742b994626baee917
Sidhant185 <74181527+Sidhant185@users.noreply.github.com>
2026-04-23 06:40:08 +0530

    Add comprehensive comments to Session-5/Q5.py

---
+++ Session-5/Q5.py
@@ -0,0 +1,12 @@
+def divideString(s, k, fill):
+    result = []
+
+    for i in range(0, len(s), k):
+        group = s[i:i+k]
+
+        if len(group) < k:
+            group += fill * (k - len(group))
+
+        result.append(group)
+
+    return result
